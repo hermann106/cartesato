09afbad049d294270a4e3d2e83fc3772485f51dd
syk1k <dambemondo@gmail.com>
2018-06-29 08:38:17 -0400

    Some media queries

--- casato/settings.py
+++ casato/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#w6qu8estji@$(a0p-d@c@08kp5)7us8jx_rgc#58lmlqgpu35'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
